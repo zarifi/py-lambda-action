6c85ebba3450eca41f9926077623def9b33bd833
zarifi <hoseinzarifi73@gmail.com>
2021-11-06 14:01:39 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,9 +29,20 @@ update_function_layers(){
 
 deploy_lambda_function(){
 	install_zip_dependencies
+	download_chrome_drivers
 	publish_dependencies_as_layer
 	publish_function_code
 	update_function_layers
+}
+
+download_chrome_drivers(){
+	# Get chromedriver
+	curl -SL https://chromedriver.storage.googleapis.com/2.32/chromedriver_linux64.zip > chromedriver.zip
+	unzip chromedriver.zip -d ./python
+
+	# Get Headless-chrome
+	curl -SL https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-29/stable-headless-chromium-amazonlinux-2017-03.zip > headless-chromium.zip
+	unzip headless-chromium.zip -d ./python
 }
 
 deploy_lambda_function
