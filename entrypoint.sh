8437e410f5d3558aab4c9a464c1901e3d4b7fc0c
zarifi <hoseinzarifi73@gmail.com>
2021-11-08 20:59:28 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,7 +41,7 @@ download_chrome_drivers(){
 	unzip chromedriver.zip -d .
 
 	# Get Headless-chrome
-	curl -SL https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-29/stable-headless-chromium-amazonlinux-2017-03.zip > headless-chromium.zip
+	curl -SL https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-55/stable-headless-chromium-amazonlinux-2017-03.zip > headless-chromium.zip
 	unzip headless-chromium.zip -d .
 	
 	# Clean
