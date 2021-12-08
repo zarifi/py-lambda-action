b8e91ce1a0d68ed51b2a36edf9ba599a3969b2b0
zarifi <hoseinzarifi73@gmail.com>
2021-12-07 22:12:38 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,7 +26,7 @@ publish_function_code(){
 
 update_function_layers(){
 	echo "Using the layer in the function..."
-	aws lambda update-function-configuration --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --layers "${INPUT_LAMBDA_LAYER_ARN}:${LAYER_VERSION}"
+	# aws lambda update-function-configuration --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --layers "${INPUT_LAMBDA_LAYER_ARN}:${LAYER_VERSION}"
 	
 }
 
